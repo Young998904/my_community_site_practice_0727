fad899f7670df7ecc7c7800dd235b3d435a2e6fe
Young998904 <like0998904@gmail.com>
2022-07-26 14:30:09 +0900

    checking written article data put by POST method

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -14,25 +14,41 @@ import java.lang.management.MemoryNotificationInfo;
 @WebServlet("/usr/*")
 public class DispatchServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         Rq rq = new Rq(req, resp);
 
         MemberController memberController = new MemberController();
         ArticleController articleController = new ArticleController();
 
-        // getRequsetURI 는 paramString 만 가지고 온다
-        String url = req.getRequestURI();
+//        // getRequsetURI 는 paramString 만 가지고 온다
+//        String url = req.getRequestURI();
 
-        switch (url) {
-            case "/usr/article/list/free" :
-                articleController.showList(rq);
+        switch (rq.getMethod()) {
+            case "GET":
+                switch (rq.getPath()) {
+                    case "/usr/article/list/free":
+                        articleController.showList(rq);
+                        break;
+                    case "/usr/article/write/free":
+                        articleController.showWrite(rq);
+                        break;
+                    case "/usr/member/login":
+                        memberController.showLogin(rq);
+                        break;
+                }
                 break;
-            case "/usr/article/write/free":
-                articleController.showWrite(rq);
-                break;
-            case "/usr/member/login" :
-                memberController.showLogin(rq);
+            case "POST":
+                switch (rq.getPath()) {
+                    case "/usr/article/write/free":
+                        articleController.doWrite(rq);
+                        break;
+                }
                 break;
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
     }
 }
